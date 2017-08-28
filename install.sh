c4c52124280f479281efc3a1998160699ee1954e
hhao99 <hhao99@gmail.com>
2017-08-28 22:31:30 +0800

    add the git config direction

--- install.sh
+++ install.sh
@@ -12,12 +12,23 @@ else
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/HomeBrew/install/master/install)";
 fi;
 brew update && cleanup;
+
 brew install zsh zsh-completion;
+
 # change the shell to zsh
 chsh -s `which zsh`
 
 # instll oh-my-zsh and copying .zshrc
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# install git and configure the git environments
+if type git > /dev/null; then
+	echo "e36mgit exists";
+else
+	brew install git
+fi;
+git config --global user.name "hhao99"
+git config --global user.email "hhao99@163.com"
 
 # initialized the brew cask
 brew cask tap
